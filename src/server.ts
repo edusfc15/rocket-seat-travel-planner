3cb548d2016785cbb6f18715483c8fcadc2c6e27
Eduardo Santana <edusc15@hotmail.com>
2024-07-09 19:26:00 -0300

    commit inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify';
+import { createTrip } from './routes/create-trip';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { confirmTrip } from './routes/confirm-trip';
+import cors from '@fastify/cors';
+
+const app = fastify();
+
+app.register(cors, {
+    origin: 'http://localhost:3333'
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+
+app.listen({ port: 3333 }).then(() => {
+    "Server is running on http://localhost:3333"
+});
